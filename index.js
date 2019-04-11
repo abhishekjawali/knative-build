c018018b3e89648b8d08843e11936db2f825f446
Abhishek Jawali <Abhishek.Jawali@team.telstra.com>
2019-04-11 15:01:27 +0530

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  const version = process.env.VERSION || "V1";
+  res.send(
+    `Welcome to Serverless Meetup @ DELLEMC. This is from Knative build! - ${version}!`
+  );
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Hello world listening on port", port);
+});
